cceb6e5feae08a7d3a33e8af94cd79a0b2fbeb39
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 09:53:51 -0700

    Time: 11 ms (35.44%), Space: 7.6 MB (38.12%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+
+class Solution 
+{
+public:
+    bool isPalindrome(string s) 
+    {
+       string ans = "";
+        //Making string with all alphanumeric and lower case characters
+        for(int i = 0; i < s.length(); i++) 
+            {
+                if(s[i] >= 65 && s[i] <= 90) 
+                {
+                    ans += tolower(s[i]);
+                }
+                else if((s[i] >= 97 && s[i] <= 122) || (s[i] >= 48 && s[i] <= 57))                     {
+                    ans += s[i];
+                }
+            }
+        int start=0,end=ans.length()-1;
+        while(start<end)
+        {
+            if(ans[start]!=ans[end])
+            {
+             return false;   
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
