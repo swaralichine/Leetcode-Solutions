57df08f644e3c0682beb6469fd3a9d848b7dc513
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 10:38:04 -0700

    Time: 0 ms (100.00%), Space: 5.8 MB (67.32%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,27 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+//Binary Search Approach
+//Time Complexity = O(logn)
+//Space Complexity = O(1)
+class Solution 
+{
+public:
+    int firstBadVersion(int n) 
+    {
+        int left=1;
+        int right=n;
+        while(left < right)
+        {
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid))
+            {
+                right=mid;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
