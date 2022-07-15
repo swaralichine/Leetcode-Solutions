63c5d986ee7df64d26a2e0a49d21ae6a950cf76a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 12:52:06 -0700

    Time: 8 ms (87.98%), Space: 13.8 MB (98.80%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    void flood_fill(vector<vector<int>>& image, int sr, int sc, int color, int col)
+    {
+        int n=image.size();
+        int m =image[0].size();
+        
+        if(sr>=n || sr<0 || sc>=m || sc<0 ||image[sr][sc]!=col)
+        {
+            return;
+        }
+        image[sr][sc]=color;
+        flood_fill(image,sr+1,sc,color,col);
+        flood_fill(image,sr-1,sc,color,col);
+        flood_fill(image,sr,sc+1,color,col);
+        flood_fill(image,sr,sc-1,color,col);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+       int col=image[sr][sc];
+       if(image[sr][sc]==color)
+       {
+           return image;
+       }
+        flood_fill(image,sr,sc,color,col);
+        return image;
+    }
+};
