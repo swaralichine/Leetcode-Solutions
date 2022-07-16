2818f288075612acc38dacb9f5cc510ac0cd169a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 20:36:05 -0700

    Time: 44 ms (71.25%), Space: 25.8 MB (94.46%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+//Two pointer technique
+//Time Complexity = O(n)
+//Space Complexity = O(1)
+class Solution 
+{
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        vector<int> result(nums.size());
+        
+        int left=0;
+        int right=nums.size()-1;
+        
+        for(int i=right;i>=0;i--)
+        {
+            int square;
+            if(abs(nums[left]) < abs(nums[right]))
+            {
+                square=nums[right];
+                right--;
+            }
+            else
+            {
+                square=nums[left];
+                left++;
+            }
+            result[i]=square*square;
+        }
+        return result;
+    }
+};
