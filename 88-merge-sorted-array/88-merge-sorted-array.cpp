c6d03f85d62e15df7724981df8edce7a4ea015ec
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 23:34:51 -0700

    Time: 4 ms (53.09%), Space: 9.1 MB (29.13%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,27 @@
+//Three pointer approach(p1,p2,p)
+//Time Complexity = O(n)
+//Space Complexity = O(1)
+class Solution 
+{
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+     int p1=m-1;
+     int p2=n-1;
+     for(int p=m+n-1;p>=0;p--)
+     {
+         if(p2<0)
+         {
+             break;
+         }
+         if(p1>=0 && nums1[p1] >nums2[p2])
+         {
+             nums1[p]=nums1[p1--];
+         }
+         else
+         {
+             nums1[p]=nums2[p2--];
+         }
+     }
+    }
+};
