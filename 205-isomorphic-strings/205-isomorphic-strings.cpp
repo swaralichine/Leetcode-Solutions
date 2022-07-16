40568efebb80b459bb49ffcbd0f511874756d916
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-15 20:14:51 -0700

    Time: 15 ms (29.23%), Space: 9.3 MB (5.03%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,27 @@
+//Time Complexity = O(n)
+//Space Complexity = O(n)
+class Solution 
+{
+public:
+    bool isIsomorphic(string s, string t) 
+    {
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
+        unordered_map<char,vector<int>> mp1,mp2;
+        for(int i=0;i<s.size();i++)
+        {
+            mp1[s[i]].push_back(i);
+            mp2[t[i]].push_back(i);
+            
+            if(mp1[s[i]] != mp2[t[i]])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
