152615e9ad11ba0b122374829835ac37a825ebb2
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-17 10:11:10 -0700

    Time: 1145 ms (9.36%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,7 +1,6 @@
 select stock_name,
-sum(case when operation='buy' then -price
-    else price
-    end) as capital_gain_loss
+sum(case when operation='sell' then price end)
+    - sum(case when operation='buy' then price end) as capital_gain_loss
     from stocks
     group by stock_name;
 
