6c4058722f87d7fa060bb3b13e22fec6a03e03ed
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-17 11:41:08 -0700

    Time: 17 ms (23.37%), Space: 12.9 MB (88.78%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,18 @@
+//Time Complexity = O(n)
+//Space Complexity = O(1)
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int maxprofit=0;
+       for(int i=1;i<prices.size();i++)
+       {
+           if(prices[i] > prices[i-1])
+           {
+               maxprofit=maxprofit + prices[i] - prices[i-1];
+           }
+       }
+        return maxprofit;
+    }
+};
