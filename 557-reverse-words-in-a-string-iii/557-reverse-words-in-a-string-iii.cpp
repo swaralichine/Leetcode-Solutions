e811f6ddf451c2f9831976791ee411dd3a2790ed
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-18 13:26:47 -0700

    Time: 21 ms (54.13%), Space: 9.7 MB (74.98%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -4,22 +4,34 @@
 class Solution 
 {
 public:
-string reverseWords(string s)
-{
-  int i = 0;
-for (int j = 0; j < s.size(); ++j)
-{
- //checking for space
-  if (s[j] == ' ') 
-  {
-   //reversing the word before the space
-    reverse(s.begin() + i, s.begin() + j);
-   //setting i to the first character after space
-    i = j + 1;
-   }
-}
-   //reversing last word after space
-     reverse(s.begin() + i, s.end());
-     return s;
-}
+    string reverseWords(string s) 
+    {
+        int left = 0, right = 0;
+
+        while (right < s.size()) 
+        {
+            if (s[right] == ' ')
+            {
+                reverseWord(s, left, right - 1);
+                left = right + 1;
+            }
+            right++;
+        }
+
+        reverseWord(s, left, right - 1);
+        
+        return s;
+    }
+
+private:
+    void reverseWord(string& s, int left, int right)
+    {
+        while (left < right) 
+        {
+            swap(s[left], s[right]);
+              left++;
+              right--;
+        }
+    }
+
 };
