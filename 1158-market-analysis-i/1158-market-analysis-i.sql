c357d7b06dd9371ebef74d9e70552c2e14753218
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-24 13:11:10 -0700

    Time: 1173 ms (63.32%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,5 +1,8 @@
-SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
-FROM users u
-LEFT JOIN Orders o
-ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
-GROUP BY u.user_id
+Select u.user_id as buyer_id, 
+u.join_date, 
+count(o.order_id) as 'orders_in_2019'
+from users u
+left join Orders o
+on o.buyer_id=u.user_id 
+and YEAR(order_date)='2019'
+group by u.user_id;
