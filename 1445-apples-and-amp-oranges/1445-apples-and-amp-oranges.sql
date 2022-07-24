b9be523d5d83b3869702e584fb6feb0fb8eb2360
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-24 15:07:32 -0700

    Time: 583 ms (19.07%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,7 +1,7 @@
-select sale_date,
+Select sale_date,
 sum(case when fruit='apples' then 1 else -1 end * sold_num) as diff 
 from sales
-group by 1
-order by 1;
+group by sale_date
+order by sale_date;
 
 
