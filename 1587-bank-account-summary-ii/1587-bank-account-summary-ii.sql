3a6320c464fbbf62ba3e499fc2aac3375df90f71
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-24 10:15:19 -0700

    Time: 1095 ms (26.10%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,7 +1,7 @@
 Select users.name,
 sum(Transactions.amount) as "balance"
 from Users
-join Transactions on Users.account=Transactions.account
+left join Transactions on Users.account=Transactions.account
 group by Users.name
 having sum(Transactions.amount)>10000;
 
