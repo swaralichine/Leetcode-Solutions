e4fd6e488291f9d7e1d6acc32c890cba847fc216
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-24 09:48:02 -0700

    Time: 12 ms (50.87%), Space: 13.7 MB (94.63%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,26 @@
+//Dynamic Programming
+//Time Complexity=O(N)
+//Space Complexity=O(1)
+class Solution 
+{
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int maxproductsofar=nums[0];
+        int minproductsofar=nums[0];
+        int maxproductsubarray=nums[0];
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            int currmaxproduct=maxproductsofar*nums[i];
+            int currminproduct=minproductsofar*nums[i];
+            
+            maxproductsofar=max(nums[i],max(currmaxproduct,currminproduct));
+            minproductsofar=min(nums[i],min(currmaxproduct,currminproduct));
+            
+            maxproductsubarray=max(maxproductsubarray,maxproductsofar);
+        }
+        return maxproductsubarray;
+    }
+};
+
