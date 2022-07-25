e5fa4fc44e09967712fb75c9337ac57d1d46311b
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-25 12:00:18 -0700

    Time: 138 ms (38.73%), Space: 27.4 MB (95.49%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,57 @@
+//Dynamic Programming
+//Time Complexity=O(Row*Column)
+//Space Complexity=O(1)
+class Solution 
+{
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat)
+    {
+        int row=mat.size();
+        if(row==0)
+        {
+            return mat;
+        }
+        int col=mat[0].size();
+        
+        vector<vector<int>> dist(row, vector<int>(col,INT_MAX-100000));  //creating a new matrix to store the updated distances
+     
+        //In first pass we check for left and top
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    dist[i][j]=0;  //no change if the distance is zero
+                }
+                else
+                {
+                    if(i>0)
+                    {
+                        dist[i][j]=min(dist[i][j],dist[i-1][j]+1);
+                    }
+                    if(j>0)
+                    {
+                        dist[i][j]=min(dist[i][j],dist[i][j-1]+1);
+                    }
+                }
+            }
+        }
+        //In second pass we check for right and down
+        for(int i=row-1;i>=0;i--)
+        {
+            for(int j=col-1;j>=0;j--)
+            {
+                if(i < row-1)
+                {
+                    dist[i][j]=min(dist[i][j],dist[i+1][j]+1);
+                }
+                if(j < col-1)
+                {
+                    dist[i][j]=min(dist[i][j],dist[i][j+1]+1);
+                }
+            }
+        }
+        return dist;
+    }
+};
