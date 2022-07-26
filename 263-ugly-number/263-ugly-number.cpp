aeea80928968b7c9cc9e8fa4899ec6a267cd0741
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-25 17:29:44 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (20.33%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    bool isUgly(int n) 
+    {
+        //loop till n is not null
+        while(n)
+        {
+            if(n==1)
+            {
+                return true;
+            }
+            if(n%2!=0 && n%3!=0 && n%5!=0)
+            {
+                return false;
+            }
+            if(n%2==0)
+            {
+                n=n/2;
+            }
+            if(n%3==0)
+            {
+                n=n/3;
+            }
+            if(n%5==0)
+            {
+                n=n/5;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
