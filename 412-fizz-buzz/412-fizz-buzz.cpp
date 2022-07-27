b9ea9a520a0dd74af1a739c148870075ff597443
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-27 15:32:21 -0700

    Time: 8 ms (27.42%), Space: 8 MB (34.06%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,31 @@
+class Solution 
+{
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> ans;
+        string ans1="FizzBuzz";
+        string ans2="Fizz";
+        string ans3="Buzz";
+        for(int i=1;i<=n;i++)
+        {
+          if(i%3==0 && i%5==0)
+          {
+              ans.push_back(ans1);
+          }
+          else if(i%3==0)
+          {
+              ans.push_back(ans2);
+          }
+          else if(i%5==0)
+          {
+              ans.push_back(ans3);
+          }
+          else
+          {
+              ans.push_back(to_string(i));
+          }
+        }
+        return ans;
+    }
+};
