ef584d7be6e8f1bd1e902a2ed5b11716d8ba8020
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-27 09:49:36 -0700

    Time: 53 ms (52.05%), Space: 10.9 MB (15.61%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,27 @@
+//Time Complexity : O(n)
+//Space Complexity : O(n)
+//Can be optimized to O(1) space complexity
+
+class Solution 
+{
+public:
+    int firstUniqChar(string s)
+    {
+          unordered_map<char,int> count;
+        for(int i=0;i<s.length();i++)
+        {
+            count[s[i]]++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(count[s[i]]==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
