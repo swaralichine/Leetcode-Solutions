2317cf9a160780c6c87be3e2b8b1a79489633b4d
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-27 14:30:27 -0700

    Time: 13 ms (78.94%), Space: 18.5 MB (36.66%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,25 +1,24 @@
-//Time Complexity = O(n)
-//Space Complexity = O(1)
-class Solution
+//Two pointer technique
+//Time Complexity=O(n)
+//Space Complexity=O(1)
+class Solution 
 {
 public:
-    int removeDuplicates(vector<int>& nums)
+    int removeDuplicates(vector<int>& nums) 
     {
+        int i=0;
         if(nums.size()==0)
         {
             return 0;
         }
-        int ans = 0;
-        for(int i=1;i<nums.size();i++)
+        for(int j=1;j<nums.size();j++)
         {
-            if(nums[i]!=nums[ans])
+            if(nums[i]!=nums[j])
             {
-                ans++;
-                nums[ans]=nums[i];
+                i++;
+                nums[i]=nums[j];
             }
         }
-        return ans+1;
+        return i+1;
     }
 };
-
-
