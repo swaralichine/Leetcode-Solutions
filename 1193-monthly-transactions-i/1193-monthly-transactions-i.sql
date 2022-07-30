618eebdc76ac2ebf08994bc20978aceae656f91d
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-29 17:30:35 -0700

    Time: 530 ms (91.22%), Space: 0B (100.00%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,18 @@
+select DATE_FORMAT(trans_date,"%Y-%m") as month,
+      country,
+      count(1) as trans_count,
+      sum(if(state="approved",1,0)) as approved_count,
+      sum(amount) as trans_total_amount,
+      sum(if(state="approved",amount,0)) as approved_total_amount
+from Transactions
+group by month,
+country
+
+
+
+
+
+
+
+
+
