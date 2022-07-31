a90ad21c4f3612d7e9ab6fa73a0ded777d0274ed
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-31 11:13:56 -0700

    Time: 36 ms (17.16%), Space: 5.8 MB (90.95%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,28 +1,22 @@
-//Time Complexity = O(n)
 class Solution 
 {
 public:
     bool isPalindrome(int x) 
     {
-        long long int rem,rev=0,temp;
-        temp=x;
-        if(x<0)
+        string s=to_string(x);
+        
+        int mid=s.length()/2;
+        
+        for(int i=0,j=s.length()-1;i<mid;i++,j--)
         {
-            return false;
+            if(s.at(i) != s.at(j))
+            {
+                return false;
+            }
         }
-        while(x>0)
-        {
-            rem=x%10;
-            rev=rev*10+rem;
-            x=x/10;
-        }
-        if(rev == temp)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return true;
     }
 };
+
+
+
