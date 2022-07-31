74108139ffdcf48e97f51b680c44ca0dabb4d216
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-07-31 10:10:55 -0700

    Time: 4 ms (28.65%), Space: 6.9 MB (72.53%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -0,0 +1,54 @@
+//Time Complexity=O(mn)
+//Space complexity=O(1)
+class Solution 
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) 
+    {
+        vector<int> ans;
+        int rows=matrix.size();
+        int col=matrix[0].size();
+        
+        //declaring four pointers
+        int up=0;
+        int left=0;
+        int down=rows-1;
+        int right=col-1;
+        
+        while(ans.size() < rows*col)
+        {
+            //traverse from left to right
+            for(int col=left;col<=right;col++)
+            {
+                ans.push_back(matrix[up][col]);
+            }
+            //traverse downwards
+            for(int row=up+1;row<=down;row++)
+            {
+                ans.push_back(matrix[row][right]);
+            }
+            //make sure we are on different row
+            if(up!=down)
+            {
+                //traverse from right to left
+                for(int col=right-1;col>=left;col--)
+                {
+                    ans.push_back(matrix[down][col]);
+                }
+            }
+            if(left!=right)
+            {
+                //traverse upwards
+                for(int row=down-1;row>up;row--)
+                {
+                    ans.push_back(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+        return ans;
+    }
+};
