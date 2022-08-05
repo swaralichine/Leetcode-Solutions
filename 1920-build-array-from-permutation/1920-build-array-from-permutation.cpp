5572d23314ee145c4c6cf200e41c1b6aba1ce6b9
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-05 09:55:35 -0700

    Time: 33 ms (20.05%), Space: 16.3 MB (56.87%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -0,0 +1,15 @@
+//Time Complexity=O(n)
+//Space Complexity=O(n)
+class Solution 
+{
+public:
+    vector<int> buildArray(vector<int>& nums) 
+    {
+        vector<int> ans(nums.size(),0);
+        for(int i=0;i<nums.size();i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+};
