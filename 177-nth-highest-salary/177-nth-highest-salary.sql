4c249976e7a165d84ff89c2caabc6febf2642a4a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-06 22:17:30 -0700

    Time: 636 ms (29.70%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set n=n-1;
+  RETURN (
+      select distinct salary
+      from employee
+      order by salary desc
+      limit 1 offset n
+  );
+END
