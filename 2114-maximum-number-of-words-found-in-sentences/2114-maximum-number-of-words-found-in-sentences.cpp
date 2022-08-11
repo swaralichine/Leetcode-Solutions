b807838ab3dd568589ac2f5b03064809c33f8b77
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-10 22:21:04 -0700

    Time: 14 ms (81.71%), Space: 9.8 MB (70.63%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int mostWordsFound(vector<string>& s)
+    {
+        int max_words = 0;
+        int i=0;
+        while(i < s.size())
+        {
+            int count = 1;
+            int j=0; 
+            while(j < s[i].length())
+            {
+                if(s[i][j++] == ' ')
+                    count++;
+            }
+            i++;
+            max_words = max(count, max_words);
+        }
+        return max_words;
+    }
+};
+
