a90c07887689a4845bd4af860b286a5ad26679b2
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-10 19:24:36 -0700

    Time: 0 ms (100.00%), Space: 13.6 MB (59.77%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    void generate(int left, int right, int n,string str,vector<string>& ans)
+    {
+        if(left==n && right==n)
+        {
+            ans.push_back(str);
+        }
+        if(left<n)
+        {
+            generate(left+1,right,n,str+'(',ans);
+        }
+        if(left>right && right<n)
+        {
+            generate(left,right+1,n,str+')',ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) 
+    {
+        vector<string> ans;
+        generate(0,0,n,"",ans);
+        return ans;
+    }
+};
