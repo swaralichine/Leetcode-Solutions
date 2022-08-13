50186b3fc628e9f93ba0d4a38bb8dc1ec8b501a6
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-13 11:34:14 -0700

    Time: 143 ms (52.83%), Space: 14.6 MB (53.55%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -5,7 +5,7 @@ class Solution
 public:
     int coinChange(vector<int>& coins, int amount) 
     {
-        vector<int>dp(amount+1,10001);
+        vector<int> dp(amount+1,amount+1);
         dp[0]=0;
         for(int i=1;i<=amount;i++)
         {
@@ -17,6 +17,9 @@ public:
               }
             }
         }
-        return dp[amount]==10001?-1:dp[amount];
+        
+        if(dp[amount]==amount+1)
+            return -1;
+        return dp[amount];
     }
 };
