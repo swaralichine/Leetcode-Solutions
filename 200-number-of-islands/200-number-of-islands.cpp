17124205eb3af7170712bdaf73fe28bb3fa3e930
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-13 12:29:41 -0700

    Time: 87 ms (13.24%), Space: 12.4 MB (55.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,3 +1,5 @@
+//time complexity=O(rc)
+//space complexity=O(rc)
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid,int x,int y)
@@ -18,15 +20,17 @@ public:
     { 
         int n=grid.size();
         int m=grid[0].size();
+        
         int noofislands=0;
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='1')
                 {
-                   noofislands++;
-                    dfs(grid,i,j);
+                    noofislands++;
+                    dfs(grid,i,j);   //check the neighboring cells to club them together
                 }
             }
         }
