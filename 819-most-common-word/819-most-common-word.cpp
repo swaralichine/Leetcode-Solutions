aa84c1384b9d2b981802c083abc30e948923bbf8
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-13 18:44:57 -0700

    Time: 7 ms (62.06%), Space: 7.8 MB (30.80%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) 
+    {
+        unordered_map<string,int> ban;
+        for(int i=0;i<banned.size();i++)
+        {
+            ban[banned[i]]++;
+        }
+        int max_freq=0;
+        string ans="";
+         unordered_map<string,int> mp;
+        for(int i=0;i<paragraph.length();i++)
+        {
+            string word="";
+            while(i<paragraph.length() && isalpha(paragraph[i]))
+            {
+                paragraph[i]=tolower(paragraph[i]);
+                word.push_back(paragraph[i]);
+                i++;    
+            }
+            if(ban.find(word)==ban.end() && word!="")
+            {
+                mp[word]++;
+                if(max_freq<mp[word])
+                {
+                    max_freq=mp[word];
+                    ans=word;
+                }
+            }
+        }
+        return ans;
+    }
+};
