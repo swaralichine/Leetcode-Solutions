ac3f66b88f3ca0202fc50757a4362a62de0f7d8b
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-14 16:30:51 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (30.48%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -5,6 +5,7 @@ class Solution
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) 
     {
+        //vector to store the answer
         vector<int> ans;
         
         int rows=matrix.size();
