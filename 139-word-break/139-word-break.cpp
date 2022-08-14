322b6a8bd57fcd0f4d1e041e787194fb8e2568ec
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-14 12:17:04 -0700

    Time: 38 ms (26.32%), Space: 13.1 MB (44.24%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,15 +2,20 @@
 //space complexity=O(n)
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
         set<string> word_set(wordDict.begin(), wordDict.end());
+        
         vector<bool> dp(s.length() + 1);
         dp[0] = true;
 
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= s.length(); i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
                 if (dp[j] and
-                    word_set.find(s.substr(j, i - j)) != word_set.end()) {
+                    word_set.find(s.substr(j, i - j)) != word_set.end()) //if word is found in the dictionary
+                {
                     dp[i] = true;
                     break;
                 }
