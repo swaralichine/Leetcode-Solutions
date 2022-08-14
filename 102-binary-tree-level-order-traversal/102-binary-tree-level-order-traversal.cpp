222cf76fc101c7516e1456fc1ae7c2a6fc0a001f
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-14 10:26:51 -0700

    Time: 8 ms (43.77%), Space: 12.4 MB (96.23%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -11,7 +11,7 @@
  */
 //BFS
 //Time Complexity = O(n)
-//Space Complexity = O(1)
+//Space Complexity = O(n)
 class Solution
 {
 public:
@@ -21,9 +21,11 @@ public:
         {
             return{};
         }
+        
         vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
+        
         while(!q.empty())
         {
             int n=q.size();
