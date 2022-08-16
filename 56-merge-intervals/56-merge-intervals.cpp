40e60d43695409c605585483e986e29f99848c21
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-16 11:50:49 -0700

    Time: 66 ms (44.81%), Space: 19.1 MB (44.86%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -14,6 +14,7 @@ public:
        vector<vector<int>> ans;
         
        ans.push_back(intervals[0]);
+        
        for(int i=1;i<intervals.size();i++)
        {
            if(ans.back()[1] >= intervals[i][0])
