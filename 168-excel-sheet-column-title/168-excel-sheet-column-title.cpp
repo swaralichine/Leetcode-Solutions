84e1544e6a5f435a010e473c4fd2a97035c0200d
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-18 19:28:26 -0700

    Time: 0 ms (100.00%), Space: 6 MB (45.39%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    string convertToTitle(int columnNumber) 
+    {
+        string ans="";
+        while(columnNumber>0)
+        {
+            columnNumber--;
+            char letter=columnNumber%26+'A';
+            ans=ans+letter;
+            columnNumber=columnNumber/26;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
