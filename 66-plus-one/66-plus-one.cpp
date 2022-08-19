41c7a42500940d22666a268e9c712dd3593d3afe
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-19 09:29:20 -0700

    Time: 0 ms (100.00%), Space: 8.6 MB (98.36%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,64 +1,32 @@
 //time complexity=O(n)
 //space complexity=O(n) //in worst case when all are nine, sc=n+1
-class Solution {
+class Solution 
+{
 public:
-    
-    /*
-        we have 3 cases here
-        1.our value is 1234
-        if last digit is less than 9, we simply add 1 to last digit and return whole number
-        return whole digits vector
-        size of vector is  not changed
-        
-        
-        2.we have value 1299
-        if last digit is 9, we substitute that value with 0 and decrement our index, till we reach
-        a value that is not 9, say less than 9, and in that case we simply increment that digit by 1
-        and return whole digits vector
-        size of vector is  not changed
-    
-    
-        3.we have value 999
-        in this case last digit is 9, we replace its value with 0, decrement our index till we reach a value < 9
-        we dont get such value and whole vector is traversed, then we simply add 1 in front of our vector
-        we simply push 1 in front of our vector and return it
-        
-        this we get to know if whole loop is traversed and havent returned anything
-    */
-    
     vector<int> plusOne(vector<int>& digits) 
     {
+        int n=digits.size();
         
-        //index will track (move) from last digit to first digit
-        int index = digits.size()-1;
-        
-        //till all elements are checked
-        //if all 9's are there in number, loop will run till all elements get its proper value
-        for(int i=index;i>=0;i--)
+        // move along the input array starting from the end
+        for(int i=n-1;i>=0;i--)
         {
-            //last digit strictly 9
-            if(digits[i] == 9)
-            {
-                //replace it with 0
-                digits[i] = 0;
-            }
+           // set all the nines at the end of array to zeros
+             if(digits[i]==9)
+             {
+                 digits[i]=0;
+             }
+            // here we have the rightmost not-nine 
             else
             {
-                //last digit less than 9 (not equals to 9)
-                //increment last digit with 1 and return last digit
-                digits[i] = digits[i] + 1;
+                digits[i]++;
+                 // and the job is done
+                //since we are supposed to increase last digits one only
                 return digits;
             }
-            //this part will be into use when last digit is 9
-            //else if less than 9, ans will be returned
         }
-        //if above loop is executed fully, and no value is thrown , means all digits where 9
-        //so all 9's have its updated value after execution of this loop
-        //now we need to add 1 at front of our ans, to fulfill correct solution 
-        
-        digits.insert(digits.begin(),1);
-        
-        return digits;
+        // we're here because all the digits are nines(corner case)
+        vector<int> ans(n+1,0);
+        ans[0]=1;
+        return ans;
     }
 };
-
