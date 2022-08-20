b2cc31729b7d191735e0a4fdfa767b6ce75e554b
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-19 23:58:58 -0700

    Time: 13 ms (53.01%), Space: 13.7 MB (17.85%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -7,15 +7,13 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {
         vector<int> ans(2);
-        ans[0]=findstartingindex(nums,target);
-        ans[1]=findendingindex(nums,target);
+        ans[0]=findfirstposition(nums,target);
+        ans[1]=findlastposition(nums,target);
         
         return ans;
     }
     
-    //First find the first position of the element
-    public:
-    int findstartingindex(vector<int>& nums, int target)
+    int findfirstposition(vector<int>& nums, int target)
     {
         int index=-1;
         int start=0;
@@ -25,25 +23,22 @@ public:
         {
             int mid=start+(end-start)/2;
             
-            if(nums[mid] >= target)
+            if(nums[mid]>=target)
             {
                 end=mid-1;
             }
-            else
+            else 
             {
                 start=mid+1;
             }
-            if(nums[mid] == target)
+            if(nums[mid]==target)
             {
                 index=mid;
             }
         }
         return index;
     }
-    
-    //Find the last position of the element
-    public :
-    int findendingindex(vector<int>& nums, int target)
+    int findlastposition(vector<int>& nums, int target)
     {
         int index=-1;
         int start=0;
@@ -53,19 +48,20 @@ public:
         {
             int mid=start+(end-start)/2;
             
-            if(nums[mid] <= target)
+            if(nums[mid]<=target)
             {
                 start=mid+1;
             }
-            else
+            else 
             {
                 end=mid-1;
             }
-            if(nums[mid] == target)
+            if(nums[mid]==target)
             {
                 index=mid;
             }
         }
         return index;
     }
+
 };
