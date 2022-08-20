6b773b35cef6b77a83814753d772a98bbf9f47af
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-20 14:37:02 -0700

    Time: 17 ms (26.43%), Space: 8.2 MB (49.41%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,38 +1,24 @@
 //time complexity=O(mn)
 //space complexity=O(1)
 //using DFS
-class Solution 
-{
+class Solution {
 public:
-    void dfs(vector<vector<char>>& board,int i,int j)
-    {
-        //checking the boundary conditions and if the cell is empty
-       if(i < 0 || j < 0 || i >= board.size() || j >=board[0].size() || board[i][j]=='.')
-       {
-           return;
-       }
-        board[i][j]='.';  //marking as visited to not visit it again
-        
-        //appllying dfs horizontal and vertical
-        dfs(board,i+1,j);
-        dfs(board,i,j+1);
-        
-    }
     int countBattleships(vector<vector<char>>& board) 
     {
-        int no_of_battleships=0;
-        for(int i=0;i<board.size();i++)
+        int ans = 0;
+        for(int i = 0; i < board.size(); i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for(int j = 0; j < board[0].size(); j++)
             {
-                if(board[i][j]=='X')
-                {
-                    no_of_battleships++;
-                    //apply dfs 
-                    dfs(board,i,j);
-                }
-             }
-         }
-        return no_of_battleships;
+                if(board[i][j] == '.')
+                    continue;
+                if(i > 0 && board[i-1][j] == 'X')
+                    continue;
+                if(j > 0 && board[i][j-1] == 'X')
+                    continue;
+                ans++;
+            }
+        }
+        return ans;
     }
 };
