7f7f27a9983ad2ea028be8e7de1366dcbd90b716
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 09:20:09 -0700

    Time: 244 ms, Memory: 93.4 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,23 @@
+//Time Complexity = O(n) only a single pass is needed
+//Space Complexity = O(1) only two variables are used
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int minprice = INT_MAX;
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            if(minprice>prices[i])
+            {
+                minprice=prices[i];
+            }
+            else if(prices[i]-minprice>maxprofit)
+            {
+                maxprofit=prices[i]-minprice;
+            }
+        }
+        return maxprofit;
+    }
+};
