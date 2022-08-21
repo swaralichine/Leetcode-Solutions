0551d662fb95657d7cb9be0e001f9679ccdb6820
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 09:57:30 -0700

    Time: 71 ms, Memory: 18.8 MB - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,31 @@
+//Time Complexity = O(nlogn) (sorting logn and n rooms)
+//space complexity=O(logn) (sorting takes this space)
+class Solution 
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+       if(intervals.size()<=1)
+       {
+           return intervals;
+       }
+       sort(intervals.begin(),intervals.end());  //sorting with start value
+        
+       vector<vector<int>> ans;
+        
+       ans.push_back(intervals[0]);
+        
+       for(int i=1;i<intervals.size();i++)
+       {
+           if(ans.back()[1] >= intervals[i][0])
+           {
+               ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+           }
+           else
+           {
+               ans.push_back(intervals[i]);
+           }
+       }
+        return ans;
+    }
+};
