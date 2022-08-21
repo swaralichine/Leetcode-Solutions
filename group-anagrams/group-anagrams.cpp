59727e2caff8e6427cd9e7d70849c48f32b6dd8c
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 16:35:06 -0700

    Time: 31 ms, Memory: 19.6 MB - LeetHub

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,25 @@
+//Time Compexity=O(nklogk) (k=max length of string)
+//space complexity=O(nk)
+class Solution 
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        unordered_map<string,vector<string>> mp;
+        int n=strs.size();
+        string temp;   //to store the original string
+        
+        for(int i=0;i<n;i++)
+        {
+            temp=strs[i];
+            sort(strs[i].begin(),strs[i].end());  //sort the current string
+            mp[strs[i]].push_back(temp);    //storing the key and the sorted                                                       array(key=sorted array)(key->temp)
+        }
+        vector<vector<string>> ans;
+        for(auto itr=mp.begin();itr!=mp.end();itr++)
+        {
+            ans.push_back(itr->second);   //pushing the original same strings
+        }
+        return ans;
+    }
+};
