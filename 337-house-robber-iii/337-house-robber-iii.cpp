19c97468ad343d9cd4330bf5289393ab17776760
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-20 22:15:03 -0700

    Time: 16 ms (86.35%), Space: 24.8 MB (59.83%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    unordered_map<TreeNode*,int> mp;
+    int rob(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        if(mp.count(root))
+        {
+            return mp[root];
+        }
+        int total=0;
+        if(root->left)   //left child exists
+        {
+            total+=rob(root->left->left)+rob(root->left->right);
+        }
+        if(root->right)   //left child exists
+        {
+            total+=rob(root->right->left)+rob(root->right->right);
+        }
+        return mp[root]=max(root->val+total, rob(root->left)+rob(root->right));
+    }
+};
