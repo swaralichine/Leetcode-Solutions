5b77f23d20c465b70c6a7f20d9a49160b7ec01f4
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 16:05:05 -0700

    Time: 3 ms, Memory: 7.1 MB - LeetHub

---
+++ rotate-image/rotate-image.cpp
@@ -0,0 +1,35 @@
+//Time Complexity=O(n*n)
+//Space Complexity=O(1)
+//First find the transpose of the matrix
+//Then reverse every row
+class Solution 
+{
+    public:
+    void rotate(vector<vector<int>>& matrix)
+    {
+
+        int n = matrix.size();
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i; j < n; j++) 
+            {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+
+        // reverse each row
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n / 2; j++) 
+            {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+};
