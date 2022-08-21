eb8f0f57217ec70918824a1bd7fc1ae729d6e01a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 11:50:47 -0700

    Time: 3 ms, Memory: 6.6 MB - LeetHub

---
+++ partition-labels/partition-labels.cpp
@@ -0,0 +1,28 @@
+//time complexity=O(n)
+//space complexity=O(1)
+class Solution 
+{
+public:
+    vector<int> partitionLabels(string s) 
+    {
+        vector<int> lastindex(26,-1);
+        //storing the last occurence of the alphabet
+        for(int i=0;i<s.size();i++)
+        {
+           lastindex[s[i]-'a']=i;                          
+        }
+        vector<int> ans;
+        int start=0;
+        int end=0;                      
+        for(int i=0;i<s.size();i++)
+        {
+           end=max(end,lastindex[s[i]-'a']);
+           if(i == end)
+           {
+               ans.emplace_back(i-start+1);
+               start=i+1;
+           }
+        }
+        return ans;
+    }
+};
