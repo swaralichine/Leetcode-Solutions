3dd3cbaee4150cb74e2e1557352952401ec1a375
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 09:16:36 -0700

    Time: 150 ms, Memory: 67.8 MB - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,27 @@
+//Kadane's algo
+//time complexity=O(n)
+//space complexity=O(1)
+class Solution 
+{
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+     int max = INT_MIN;
+     int sum=0;
+        
+      for(int i=0;i<nums.size();i++)
+      {
+        sum=sum+nums[i];
+        if(sum > max)
+        {
+            max=sum;
+        }
+        if(sum < 0)
+        {
+            sum=0;
+        }
+      }
+        return max;
+    }
+};
+
