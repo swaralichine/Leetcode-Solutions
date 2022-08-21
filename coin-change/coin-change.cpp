0acd069927b13d06c2a62cbdd9b1760ce6941e8e
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 09:33:46 -0700

    Time: 166 ms, Memory: 14.5 MB - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,25 @@
+//time complexity=O(s*n) s=amount,n=denomintaion
+//space complexity=O(s) extra space for memoization table
+//make a table
+class Solution 
+{
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=0;j<coins.size();j++)
+            {
+              if(i>=coins[j])
+              {
+                  dp[i]=min(dp[i],dp[i-coins[j]]+1);
+              }
+            }
+        }
+        if(dp[amount]==amount+1)
+            return -1;
+        return dp[amount];
+    }
+};
