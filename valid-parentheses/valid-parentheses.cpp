0c23acd9d3eb11897d7a2401af962fb1661bb300
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 17:17:28 -0700

    Time: 6 ms, Memory: 6.3 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,41 @@
+//time complexity=O(n)
+//space complexity=O(n)
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        for(char ch:s)
+        {
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                if(ch == ')' && st.top() != '(')
+                {
+                    return false;
+                }
+                if(ch == '}' && st.top() != '{')
+                {
+                    return false;
+                }
+                if(ch == ']' && st.top() != '[')
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
