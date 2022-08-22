7a28bac00d784da4dd46a30b3d6494a85d98f82a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-22 12:20:53 -0700

    Time: 57 ms (51.08%), Space: 71.5 MB (51.41%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -39,6 +39,7 @@ public:
                 sum=sum+carry;
             }
             carry=sum/10;
+            
             ListNode* node=new ListNode(sum%10);
             temp->next=node;
             temp=temp->next;
