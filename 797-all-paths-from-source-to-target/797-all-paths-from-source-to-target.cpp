21c2a2911bf7422262e6ec816f0da76bef99a4dc
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-22 08:41:17 -0700

    Time: 27 ms (48.35%), Space: 10.5 MB (98.70%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -2,25 +2,30 @@
 class Solution
 {
 public:
-    void dfs(vector<vector<int>>&graph, vector<vector<int>>&res, vector<int> &path, int cur){
+    void dfs(vector<vector<int>>&graph, vector<vector<int>>&res, vector<int> &path, int cur)
+    {
     
-    path.push_back(cur);
-    if(cur == graph.size()-1){
-        res.push_back(path);
-    }
-       else {
-           for(auto it : graph[cur]){
+    path.push_back(cur);   //storing the element
+        
+        if(cur == graph.size()-1)   //reached last node
+        {
+            res.push_back(path);
+        }
+       else 
+       {
+           for(auto it : graph[cur])
+           {
                dfs(graph, res, path, it);
            }
        }
        path.pop_back();
-}
-vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-    vector<vector<int>> res;
-    vector<int> path;
-    
-    dfs(graph,res, path, 0);
-    return res;
-}
+    }
+   vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
+    {
+        vector<vector<int>> res;
+        vector<int> path;
 
+        dfs(graph,res, path, 0);
+        return res;
+    }
 };
