cc60d19588b8738b36dc8f7e1dd87cb11328c24e
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 17:08:42 -0700

    Time: 26 ms, Memory: 17.9 MB - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+      int n = nums.size();
+      int sum = n*(n+1)/2;
+        int sum1=0;
+      for(int i=0;i<nums.size();i++)
+      {
+          sum1=sum1+nums[i];
+      }
+        return sum-sum1;
+    }
+};
