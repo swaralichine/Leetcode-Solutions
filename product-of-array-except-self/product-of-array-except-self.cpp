15757944bf492a5ab2232bd38795d10740c5d955
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 17:02:14 -0700

    Time: 57 ms, Memory: 24.8 MB - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,34 @@
+//time complexity=O(n)
+//space complexity=O(1)
+//ans[i]=left[i-1]*right[i+1]
+class Solution 
+{
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int> ans;
+        
+        if(n<1)
+        {
+            return ans;
+        }
+        int product=1;
+        //we traverse from left to right and find the cumulative multiplication
+        for(int i=0;i<nums.size();i++)
+        {
+            product*=nums[i];
+            ans.push_back(product);
+        }
+        //traverse from right and update the ans vector
+        product=1;
+        for(int i=n-1;i>0;i--)
+        {
+            ans[i]=ans[i-1]*product;
+            product*=nums[i];
+        }
+        ans[0]=product;   //corner case
+        
+        return ans;
+    }
+};
