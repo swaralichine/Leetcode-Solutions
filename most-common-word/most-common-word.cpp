e61bbbfb015461099771f4f68a32cf020e57be6d
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-21 17:27:02 -0700

    Time: 7 ms, Memory: 7.6 MB - LeetHub

---
+++ most-common-word/most-common-word.cpp
@@ -0,0 +1,41 @@
+//time complexity=O(n+m) n:no of characters in input string, m: no of characters in banned list
+//space complexity=O(n+m)
+class Solution
+{
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) 
+    {
+        unordered_map<string,int> ban;
+        for(int i=0;i<banned.size();i++)
+        {
+            ban[banned[i]]++;
+        }
+        
+        int max_freq=0;
+        string ans="";
+        unordered_map<string,int> mp;
+        
+        for(int i=0;i<paragraph.length();i++)
+        {
+            string word="";
+            
+            while(i<paragraph.length() && isalpha(paragraph[i]))
+            {
+                paragraph[i]=tolower(paragraph[i]);
+                word.push_back(paragraph[i]);
+                i++;    
+            }
+            //if word is not there in banned list then only increase its frequency
+            if(ban.find(word)==ban.end() && word!="")
+            {
+                mp[word]++;
+                if(max_freq<mp[word])
+                {
+                    max_freq=mp[word];
+                    ans=word;
+                }
+            }
+        }
+        return ans;
+    }
+};
