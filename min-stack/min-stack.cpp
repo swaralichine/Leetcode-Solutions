593bdd222edcdea6db9755170fdfa17203c14294
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-22 22:24:13 -0700

    Time: 54 ms, Memory: 16.4 MB - LeetHub

---
+++ min-stack/min-stack.cpp
@@ -0,0 +1,39 @@
+//time complexity=O(1)
+//space complexity=O(n)
+
+class MinStack 
+{
+   vector<pair<int,int>> v; //to store the element and the minimum element
+public:
+    MinStack() 
+    {
+    
+    }
+    
+    void push(int val) 
+    {
+        if(v.size()==0)
+        {
+            v.push_back({val,val});
+        }
+        else
+        {
+            v.push_back({val,min(val,v[v.size()-1].second)});  //second                                                     element is the minimum
+        }
+    }
+    
+    void pop() 
+    {
+        v.pop_back();
+    }
+    
+    int top() 
+    {
+        return v[v.size()-1].first;
+    }
+    
+    int getMin() 
+    {
+        return v[v.size()-1].second;
+    }
+};
