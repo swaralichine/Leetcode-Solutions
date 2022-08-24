a80d32ca1ffce3c49a5b3169dff3d9c99c19f4f7
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-24 14:23:47 -0700

    Time: 12 ms, Memory: 12.1 MB - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -10,14 +10,14 @@ public:
         
         for(int i=0;i<nums.size();i++)
         {
-            if(mp.find(target-nums[i])!=mp.end())
-            {
-               ans.push_back(mp[target-nums[i]]);
-               ans.push_back(i);
-            }
-            mp[nums[i]]=i;
+            if(mp.find(target-nums[i]) != mp.end())
+               {
+                   ans.push_back(mp[target-nums[i]]);
+                   ans.push_back(i);
+               }
+               mp[nums[i]]=i;
         }
-        return ans;
+               return ans;
     }
 };
 
