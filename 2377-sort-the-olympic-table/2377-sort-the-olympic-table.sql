64e7b600c2be7ffd1778778436934ae96b34ae29
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-27 12:22:12 -0700

    Time: 337 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 2377-sort-the-olympic-table/2377-sort-the-olympic-table.sql
@@ -0,0 +1,12 @@
+Select  country,
+        gold_medals,
+        silver_medals,
+        bronze_medals
+from Olympic
+order by gold_medals desc,
+         silver_medals desc,
+         bronze_medals desc,
+         country asc;
+
+
+
