8f5b830643687632e1635129c8ed18be8e2c3e1c
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-31 21:55:35 -0700

    Time: 114 ms (82.04%), Space: 53.4 MB (23.56%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,16 +1,19 @@
+//time complexity=O(n)  
+//space complexity=O(n)  hashtable is linear
 class Solution 
 {
 public:
     bool containsDuplicate(vector<int>& nums) 
     {
         unordered_map<int,int> count;
+        
         for(auto x:nums)
         {
             count[x]++;
         }
         for(auto x:count)
         {
-            if(x.second >=2)
+            if(x.second >=2)   //it means it contains duplicates
             {
                 return true;
             }
