5179cb60b9433ace2a4dcecc853918229cfdea7e
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-31 21:18:22 -0700

    Time: 159 ms (82.65%), Space: 86.4 MB (54.67%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,43 @@
+
+class Solution {
+public:
+    
+    int count = 0;
+    
+    void dfs(TreeNode* root, int maxi)
+    {
+        // base case
+        
+        if(root == NULL)
+            return ;
+        
+        // if max. value of ancestors is less than or equal to curr node then increment count
+        
+        if(root -> val >= maxi)
+        {
+            count++;
+        }
+        
+        // update maxi
+        
+        maxi = max(maxi, root -> val);
+        
+        // call for left subtree
+        
+        dfs(root -> left, maxi);
+        
+        // call for right subtree
+        
+        dfs(root -> right, maxi);
+    }
+    
+    int goodNodes(TreeNode* root) {
+        
+        if(root == NULL)
+            return 0;
+        
+        dfs(root, root -> val);
+        
+        return count;
+    }
+};
