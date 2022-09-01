03de4ca8cf9fcefba7f325dfe389cdd9e25e1b79
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-31 22:00:24 -0700

    Time: 112 ms (97.02%), Space: 67.7 MB (90.67%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -7,11 +7,12 @@ public:
     int maxSubArray(vector<int>& nums) 
     {
      int max = INT_MIN;
-     int sum=0;
+     int sum = 0;
         
       for(int i=0;i<nums.size();i++)
       {
         sum=sum+nums[i];
+          
         if(sum > max)
         {
             max=sum;
