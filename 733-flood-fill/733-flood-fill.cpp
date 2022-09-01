ac8b034e36c7366181039dc8d7ab5fdf37e19624
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-31 22:46:00 -0700

    Time: 18 ms (37.89%), Space: 13.9 MB (90.27%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,4 +1,4 @@
-//Time Complexity=O(n)
+//Time Complexity=O(n) n is the number of pixels in the image
 //Space Complexity=O(n)
 class Solution 
 {
@@ -8,11 +8,13 @@ public:
         int n=image.size();
         int m =image[0].size();
         
-        if(sr>=n || sr<0 || sc>=m || sc<0 ||image[sr][sc]!=col)
+        if(sr>=n || sr<0 || sc>=m || sc<0 ||image[sr][sc]!=col)    //checking the boundary conditions
         {
             return;
         }
-        image[sr][sc]=color;
+        image[sr][sc]=color;  //color the cell
+        
+        //traverse the neighboring cells using dfs
         flood_fill(image,sr+1,sc,color,col);
         flood_fill(image,sr-1,sc,color,col);
         flood_fill(image,sr,sc+1,color,col);
@@ -20,10 +22,11 @@ public:
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
     {
-       int col=image[sr][sc];
-       if(image[sr][sc]==color)
+       int col=image[sr][sc];   //storing the color of the starting pixel
+        
+       if(image[sr][sc]==color)     //the starting/target pixel is same as the color to be filled with
        {
-           return image;
+           return image;     //no need to change anything, directly return the image
        }
         flood_fill(image,sr,sc,color,col);
         return image;
