80451b00b340c8d166e2c83703b6ad085505f658
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-08-31 22:08:45 -0700

    Time: 724 ms (60.62%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,9 +1,7 @@
 Select prices.product_id,
-round(sum(prices.price*UnitsSold.units)/sum(UnitsSold.units),2) as
-average_price 
+round(sum(prices.price*UnitsSold.units)/sum(UnitsSold.units),2) as average_price 
 from prices
-inner join UnitsSold
-on prices.product_id = UnitsSold.product_id and 
+inner join UnitsSold on prices.product_id = UnitsSold.product_id and 
 UnitsSold.purchase_date between start_date and end_date
 group by prices.product_id;
 
