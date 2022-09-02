eb44ed40dc962d1f470f6a988452338491963e83
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 20:10:34 -0700

    Time: 39 ms (75.74%), Space: 25 MB (57.09%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,7 +6,9 @@ public:
     void rotate(vector<int>& nums, int k) 
     {
         int n=nums.size();
+        
         k=k%n;   //if k is greater than n
+        
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
