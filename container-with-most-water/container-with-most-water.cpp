4f93f731d2d5dd59b31abaeba0c8b834062835aa
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 17:51:57 -0700

    Time: 265 ms, Memory: 59.1 MB - LeetHub

---
+++ container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,27 @@
+//Two Pointer Approach
+//time complexity=O(n)
+//Space complexity=O(1)
+class Solution 
+{
+public:
+    int maxArea(vector<int>& height) 
+    {
+       int max_water=0;
+       int left=0;
+        int right=height.size()-1;
+        while(left < right)
+        {
+            int width=right-left;
+            max_water= max(max_water , min(height[left],height[right]) * width);
+            if(height[right] > height[left])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return max_water;
+    }
+};
