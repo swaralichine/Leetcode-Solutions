71fc291c42d89687d19b44063e7bd2d922fd9f76
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 21:14:40 -0700

    Time: 871 ms (16.93%), Space: 0B (100.00%) - LeetHub

--- 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -1,5 +1,5 @@
 select round(
-       count(case when customer_pref_delivery_date=order_date then       delivery_id end)*100 
+count(case when customer_pref_delivery_date=order_date then delivery_id end)*100 
            /
           count(delivery_id)
           ,2) as immediate_percentage
