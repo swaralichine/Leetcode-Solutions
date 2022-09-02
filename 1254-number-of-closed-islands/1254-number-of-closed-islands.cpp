207f42f10eedf80ee26fbf1535180e630f8f7d78
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 23:45:57 -0700

    Time: 33 ms (13.77%), Space: 9.4 MB (92.96%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,19 +1,21 @@
 //DFS Approach
+//time co
 class Solution 
 {
 public:
     bool dfs(vector<vector<int>>& grid,int i,int j)
     {
-        if(i < 0 || j < 0 || i>=grid.size()||j>=grid[0].size())
+        if(i < 0 || j < 0 || i>=grid.size() || j>=grid[0].size())
         {
             return false;
         }
-        if(grid[i][j]==1)
+        if(grid[i][j]==1)   //if it is water
         {
             return true;
         }
-        grid[i][j]=1;
+        grid[i][j]=1;   //mark visited
         
+        //dfs travel
         bool up=dfs(grid,i-1,j);
         bool down=dfs(grid,i+1,j);
         bool right=dfs(grid,i,j+1);
@@ -25,12 +27,14 @@ public:
     {
         int n=grid.size();
         int m=grid[0].size();
+        
         int noofclosedislands=0;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
+                //we need to check both conditions:whether its a land and if yes whether it is surrounded by water
                 if(grid[i][j]==0 && dfs(grid,i,j)==true)
                 {
                     noofclosedislands++;
