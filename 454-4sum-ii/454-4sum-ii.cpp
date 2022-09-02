1dcd942e45754621cfcabd738e4c4b5dcd368470
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 17:04:54 -0700

    Time: 535 ms (23.34%), Space: 24.3 MB (95.87%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,22 +1,32 @@
+//time complexity can be improved
+//used hashmap
 class Solution 
 {
 public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-		int n=nums1.size(),answer=0;
-		unordered_map<int,int>mp;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				mp[nums3[i]+nums4[j]]++;
-			}
-		}
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				if(mp.count(-(nums1[i]+nums2[j]))){
-					answer+=mp[-(nums1[i]+nums2[j])];
-				}
-			}
-		}
-		return answer;
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) 
+    {
+        int n=nums1.size();
+        int ans=0;
+	   //declaring a hashmap
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                mp[nums1[i]+nums2[j]]++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mp.count(-(nums3[i]+nums4[j])))
+                {
+                    ans=ans+mp[-(nums3[i]+nums4[j])];
+                }
+            }
+        }
+        return ans;
 	}
 };
-
