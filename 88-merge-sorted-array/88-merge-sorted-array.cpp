6756351f6fd3435214bf89ac3e17c74439d5f911
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 20:45:48 -0700

    Time: 7 ms (29.06%), Space: 9.1 MB (72.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -8,19 +8,22 @@ public:
     {
      int p1=m-1;
      int p2=n-1;
-     for(int p=m+n-1;p>=0;p--)
+        
+     for(int i=m+n-1;i>=0;i--)
      {
          if(p2<0)
          {
              break;
          }
-         if(p1>=0 && nums1[p1] >nums2[p2])
+         if(p1>=0 && nums1[p1] > nums2[p2])
          {
-             nums1[p]=nums1[p1--];
+             nums1[i]=nums1[p1];
+             p1--;
          }
          else
          {
-             nums1[p]=nums2[p2--];
+             nums1[i]=nums2[p2];
+             p2--;
          }
      }
     }
