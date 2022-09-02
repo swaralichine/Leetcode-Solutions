fa64f097ae870bc4d3c3e47287265a031f2fc950
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-01 17:51:35 -0700

    Time: 271 ms (5.02%), Space: 59 MB (36.77%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,12 +1,16 @@
 //Two Pointer Approach
+//time complexity=O(n)
+//Space complexity=O(1)
 class Solution 
 {
 public:
     int maxArea(vector<int>& height) 
     {
        int max_water=0;
+        
        int left=0;
-        int right=height.size()-1;
+       int right=height.size()-1;
+        
         while(left < right)
         {
             int width=right-left;
