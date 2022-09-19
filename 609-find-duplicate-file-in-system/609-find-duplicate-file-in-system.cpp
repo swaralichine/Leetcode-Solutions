50e99b2611eeadda9a1c0c05a17627db297a0129
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-19 10:08:48 -0700

    Time: 278 ms (12.59%), Space: 43.3 MB (38.93%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -6,25 +6,31 @@ class Solution
 public:
     vector<vector<string>> findDuplicate(vector<string>& paths) 
     {   
-        map<string,vector<string>> maap;
+        map<string,vector<string>> mp;
         
-        for(int i = 0; i < paths.size(); i++){
+        for(int i = 0; i < paths.size(); i++)
+        {
             string t,dir;
             bool flag = true;
             
             // istringstream breaks down a string into words around spaces
             istringstream ss(paths[i]);
             
-            while(ss>>t){
-                if(flag){
+            while(ss>>t)
+            {
+                if(flag)
+                {
                     // *dir stores first part of the string which is directory
                     dir = t;
                     flag = false;
                 }
-                else{
+                else
+                {
                     string k = "";
-                    for(int j = t.size()-2; j > 0; j--){
-                        if(t[j] == '('){
+                    for(int j = t.size()-2; j > 0; j--)
+                    {
+                        if(t[j] == '(')
+                        {
                             /* use fn_content as key to store values in map
                             For ex: root/a 1.txt(abcd) 2.txt(efgh)
                             string t = { root/a, 1.txt(abcd), 2.txt(efgh)}
@@ -33,23 +39,25 @@ public:
                             maap[abcd].push_back(root/a + "/" + 1.txt);
                             maap[efgh].push_back(root/a + "/" + 2.txt);
                             */
-                            maap[k].push_back(dir+"/"+t.substr(0,j));
+                            mp[k].push_back(dir+"/"+t.substr(0,j));
                             break;
                         }
-                        k += t[j];
+                        k = k + t[j];
                     }
                 }
             }
         }
         
         vector<vector<string>> ans;
-        for(auto it:maap){
+        
+        for(auto it:mp)
+        {
             // if size > 1,that's when duplicate exists
             if(it.second.size()>1)
-                ans.push_back(it.second);
+            {
+              ans.push_back(it.second);   
+            }
         }
-        
         return ans;
-        
     }
 };
