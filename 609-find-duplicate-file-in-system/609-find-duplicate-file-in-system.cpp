9612f74ee29aab247b5333601ac13142ee198241
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-19 08:15:47 -0700

    Time: 119 ms (89.69%), Space: 41.3 MB (48.85%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
- vector<vector<string>> findDuplicate(vector<string>& paths) {
-    unordered_map<string, vector<string>> umtostore;
-    vector<vector<string>> ans;
-
-    for (auto i : paths) {
-	    stringstream stream(i);
-	    string filelocation;
-	    string s;
-	    getline(stream, filelocation, ' ');
-	    while (getline(stream, s, ' ')) {
-		    string fname = filelocation + '/' + s.substr(0, s.find('('));
-		    string fcontent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
-		    umtostore[fcontent].push_back(fname);
-	    }
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        map<string,vector<string>> maap;
+        
+        for(int i = 0; i < paths.size(); i++){
+            string t,dir;
+            bool flag = true;
+            
+            // istringstream breaks down a string into words around spaces
+            istringstream ss(paths[i]);
+            
+            while(ss>>t){
+                if(flag){
+                    // *dir stores first part of the string which is directory
+                    dir = t;
+                    flag = false;
+                }
+                else{
+                    string k = "";
+                    for(int j = t.size()-2; j > 0; j--){
+                        if(t[j] == '('){
+                            /* use fn_content as key to store values in map
+                            For ex: root/a 1.txt(abcd) 2.txt(efgh)
+                            string t = { root/a, 1.txt(abcd), 2.txt(efgh)}
+                            string dir = "root/a"
+                            string k = {abcd,efgh}
+                            maap[abcd].push_back(root/a + "/" + 1.txt);
+                            maap[efgh].push_back(root/a + "/" + 2.txt);
+                            */
+                            maap[k].push_back(dir+"/"+t.substr(0,j));
+                            break;
+                        }
+                        k += t[j];
+                    }
+                }
+            }
+        }
+        
+        vector<vector<string>> ans;
+        for(auto it:maap){
+            // if size > 1,that's when duplicate exists
+            if(it.second.size()>1)
+                ans.push_back(it.second);
+        }
+        
+        return ans;
+        
     }
-
-    for (auto file : umtostore) {
-	    if (file.second.size() > 1)
-		    ans.push_back(file.second);
-    }
-
-    return ans;
-}
 };
