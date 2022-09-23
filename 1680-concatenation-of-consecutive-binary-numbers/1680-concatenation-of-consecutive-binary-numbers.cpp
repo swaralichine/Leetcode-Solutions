c47085716a8a61d71220d60d56dabd7b68294a93
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-22 20:51:07 -0700

    Time: 33 ms (93.48%), Space: 5.8 MB (82.61%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,23 @@
+//time complexity=O(n)
+//space complexity = O(1)
+
+class Solution 
+{
+public:
+    int concatenatedBinary(int n) 
+    {
+       int mod=1e9 +7;
+       long long ans=0;
+       int len=0;
+       
+       for(int i=1;i<=n;i++)
+       {
+           if((i & (i-1)) == 0)  
+           {
+               len++;
+           }
+           ans = ((ans << len) + i) % mod;  //adding the next number binary
+       }
+        return ans;
+    }
+};
