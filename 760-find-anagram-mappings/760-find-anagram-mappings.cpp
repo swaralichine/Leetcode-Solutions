2df452235da81d11b1151a5f639620df4e921271
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-25 10:36:53 -0700

    Time: 5 ms (48.51%), Space: 8.9 MB (15.35%) - LeetHub

---
+++ 760-find-anagram-mappings/760-find-anagram-mappings.cpp
@@ -0,0 +1,23 @@
+//using hashmap
+//time complexity=O(n)
+//space complexity=O(n)
+class Solution 
+{
+public:
+    vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) 
+    {
+       vector<int> ans=nums1;
+       map<int,int> mp;
+        
+       for(int i=0;i<nums2.size();i++)
+       {
+           mp[nums2[i]]=i;
+       }
+        for(int i=0;i<nums1.size();i++)
+        {
+            ans[i]=mp[nums1[i]];  //index of nums1 in map(map stores nums2)
+        }
+        return ans;
+    }
+};
+
