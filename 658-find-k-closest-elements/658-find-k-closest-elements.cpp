2322d5c96ba100f90523cf3de605de38644c5cab
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-29 07:16:50 -0700

    Time: 211 ms (11.09%), Space: 34 MB (37.52%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -4,21 +4,24 @@ class Solution
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) 
     {
+        //using max-heap so that it will store the closest k elements
         priority_queue<pair<int,int>> pq;
         
         for(int num:arr)
         {
             int diff=abs(num-x);
-            pq.push({diff,num});
+            pq.push({diff,num});  //storing the diff and the number associated with it
             if(pq.size()>k)
             {
                 pq.pop();
             }
         }
+        //declaring answer vector 
         vector<int> ans;
+        
         while(!pq.empty())
         {
-            int temp=pq.top().second; //second element is the number(first is                                         the difference)
+            int temp=pq.top().second; //second element is the number(first is                                                                               the difference)
             pq.pop();
             ans.push_back(temp);
         }
