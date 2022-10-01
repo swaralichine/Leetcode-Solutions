682a45c504bae09a64a607099c92037aadc664c2
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-01 00:40:02 -0700

    Time: 1 ms (42.52%), Space: 6 MB (25.69%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,17 +1,18 @@
 //Time Complexity=O(1)
-//Space Complexity=O(1)
+//Space Complexity=O(n)
 
-class Solution 
+class Solution
 {
 public:
-    int getSum(int a, int b) 
-    {
-        while(b!=0)
-        {
-            unsigned c=a&b;
-            a=a^b;
-            b=c<<1;
-        }
-        return a;
-    }
+int getSum(int a, int b) 
+ {
+    vector<int> v;
+    //putting both a and b in a dynamic array
+    v.push_back(a);
+    v.push_back(b);
+    
+    //calculating the sum of numbers between beginning and end position , with inital value of sum taken as "zero"
+    int ans = accumulate(v.begin(),v.end(),0);
+    return ans;
+ }
 };
