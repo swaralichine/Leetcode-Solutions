9e7e1918ee16c3ee0a17d9af87c10421189926f4
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-01 01:07:02 -0700

    Time: 4 ms (51.19%), Space: 6 MB (51.01%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,20 +1,23 @@
 //time complexity=O(1)
 //space complexity=O(1)
+//number n is traversed from right to left
 class Solution 
 {
 public:
     uint32_t reverseBits(uint32_t n) 
     {
-        int result=0;
+        int ans=0;
+        
         for(int i=0;i<32;i++)
         {
-            result=result<<1;
-            if(n&1>0)
+            ans = ans << 1;   //shift the ans to left
+            
+            if(n & 1 > 0)
             {
-                result++;
+                ans++; 
             }
-            n=n>>1;
+            n = n >> 1;  //right shift the number
         }
-        return result;
+        return ans;
     }
 };
