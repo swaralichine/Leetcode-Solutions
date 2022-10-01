810496e42421cedc0592f71f03180ea826cd20d7
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-09-30 21:52:44 -0700

    Time: 0 ms (100.00%), Space: 6.2 MB (75.45%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,36 +1,40 @@
-//DP approach
-//Time Complexity=O(n)
-//Space Complexity=O(1)
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+// This solution can be further space-optimized to be O(1). Let's think about it :D
 class Solution 
 {
 public:
+    // number of ways to do something -> think about dp
     int numDecodings(string s) 
     {
-        if (s[0] == '0') 
-        {
+        // cannot map to any character due to the leading zero
+        if (s.front() == '0') 
             return 0;
-        }
-
-        size_t n = s.length();
-        int two_back = 1;
-        int one_back = 1;
-
-        for (size_t i = 1; i < n; i++) 
+        int n = s.size(); 
+        // dp[i]: number of ways of decoding the substring s[:i]
+        vector<int> dp(n + 1);
+        // base case
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++)
         {
-            int current = 0;
-            if (s[i] != '0')
+            // check single digit decode
+            // valid deocde is possible only when s[i - 1] is not zero
+            // if so, take the previous state dp[i - 1]
+            // e.g. AB - 1[2]
+            if (s[i - 1] != '0') dp[i] = dp[i - 1];
+            // check double digit decode
+            // by looking at the previous two digits
+            // if the substring belongs to the range [10 - 26]
+            // then add the previous state dp[i - 2]
+            // e.g. L - [12]
+            if (i >= 2) 
             {
-                current = one_back;
+                // or you can use `stoi(s.substr(i - 2, 2))`
+                int x = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
+                // check the range
+                if (10 <= x && x <= 26) dp[i] += dp[i - 2];
             }
-            int two_digit = stoi(s.substr(i - 1, 2));
-            if (two_digit >= 10 and two_digit <= 26)
-            {
-                current += two_back;
-            }
-
-            two_back = one_back;
-            one_back = current;
         }
-        return one_back;
+        return dp[n];
     }
 };
