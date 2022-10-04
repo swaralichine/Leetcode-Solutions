2fd21d1cc53c1dc15013a2341e4ae91d756b6a9a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-04 08:43:41 -0700

    Time: 11 ms (89.08%), Space: 21.3 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -7,24 +7,38 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ * };**/
 class Solution 
 {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) 
+    
+    bool pathSum(TreeNode* root, int targetSum, int currSum) 
     {
-        if(root == NULL)
+        
+        if (root == NULL)  
         {
-            return false;
+            return false; 
         }
-        if(root->val == targetSum && root->left == NULL && root->right == NULL)
+
+        currSum = currSum + root->val;
+        
+        if ((root->left == NULL && root->right == NULL) && targetSum == currSum)
         {
             return true;
         }
-        bool left = hasPathSum(root->left,targetSum-root->val);
-        bool right = hasPathSum(root->right,targetSum-root->val);
+        
+        bool left = pathSum(root->left, targetSum, currSum);
+        bool right = pathSum(root->right, targetSum, currSum);
         
         return left || right;
+    }
+    
+    
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        int currSum = 0;
+        bool ans = pathSum(root, targetSum, currSum);
+        
+        return ans;
     }
 };
