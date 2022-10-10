bacec1361b5c9345312a1157a3c10da61506b242
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-10 07:47:26 -0700

    Time: 3 ms (45.61%), Space: 6 MB (95.35%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,25 @@
+//time complexity=O(n)
+//space complexity=O(n)
+class Solution 
+{
+public:
+    string breakPalindrome(string palindrome) 
+    {
+        int n = palindrome.size();
+        
+        if(n == 1)
+        {
+            return "";
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            if(palindrome[i] != 'a')
+            {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome[n - 1] = 'b';
+        return palindrome;
+    }
+};
