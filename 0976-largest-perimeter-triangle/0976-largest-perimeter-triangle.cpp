89601667017d7243ea257eb89e5bcdd12b8de136
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-12 08:38:03 -0700

    Time: 87 ms (25.11%), Space: 21.7 MB (96.06%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,19 @@
+//Time Complexity = O(nlogn)
+//Space Complexity = O(1)
+class Solution 
+{
+public:
+    int largestPerimeter(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        
+        for(int i=nums.size()-3;i>=0;i--)
+        {
+            if(nums[i]+nums[i+1] > nums[i+2])
+            {
+                return nums[i]+nums[i+1]+nums[i+2];
+            }
+        }
+        return 0;
+    }
+};
