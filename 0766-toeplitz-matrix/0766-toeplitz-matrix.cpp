17e7fd8045dd0b9dc2dbbdb6a46ea54c795e662c
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2022-10-30 19:43:45 -0700

    Time: 27 ms (66.19%), Space: 17.3 MB (85.40%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix)
+    {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        
+        for(int i=1;i<r;i++)
+        {
+            for(int j=1;j<c;j++)
+            {
+                if(matrix[i][j]!=matrix[i-1][j-1])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
