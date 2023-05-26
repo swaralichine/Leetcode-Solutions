26d1bf01dcca0090f8c89df843a9d805a8a67451
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-05-26 10:37:08 -0700

    Time: 12 ms (58.44%), Space: 13.5 MB (84.42%) - LeetHub

---
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -0,0 +1,23 @@
+//time complexity=O(n)
+//space complexity=O(1)
+
+class Solution
+{
+public:
+    void swap(vector<int> &nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    void wiggleSort(vector<int>& nums)
+    {
+       for(int i=0;i<nums.size()-1;i++)
+       {
+           if(((i%2 == 0) && nums[i] > nums[i+1]) || (i%2 == 1) && nums[i] < nums[i+1])
+           {
+               swap(nums,i,i+1);
+           }
+       }
+    }
+};
