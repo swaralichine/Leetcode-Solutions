c26bacf2d4adb282eddb52d1cdd4a61ec0b21664
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-06-19 13:13:16 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (44.32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -5,11 +5,11 @@ public:
     {
         int count = 0;
         int i=s.length()-1; //start from end
-        while(i>=0 && s[i] == ' ')
+        while(i>=0 && s[i] == ' ')    //trimming white spaces
         {
             i--;
         }
-        while(i>=0 && s[i] != ' ')
+        while(i>=0 && s[i] != ' ')     //calculate the length of the last word now that it is found
         {
             count++;
             i--;
