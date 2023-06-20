880d152cf10b8a7691ef8f9e9feef928bb70f5b6
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-06-20 08:29:48 -0700

    Time: 42 ms (62.99%), Space: 28.2 MB (74.59%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,38 @@
+//Using two pointers
+//Time Complexity=O(n)
+//Space Complexity=O(1)
+//Take two pointers:left and right
+//Start adding the nums to sum
+//When the sum >=target, then start finding the min length 
+//Also start subtracting nums[left] from the sum
+class Solution 
+{
+public:
+    int minSubArrayLen(int target, vector<int>& nums)
+    {
+        int n=nums.size();
+        int left=0;
+        int right=0;
+        int sum=0;
+        int len=INT_MAX;
+        
+        while(right<n)
+        {
+            sum=sum+nums[right++];
+            while(sum>=target)
+            {
+                len=min(len,right-left);   //right - left is to find the length
+                sum=sum-nums[left++];
+            }
+        }
+        
+        if(len==INT_MAX)
+        {
+            return 0;
+        }
+        else
+        {
+            return len;
+        }
+    }
+};
