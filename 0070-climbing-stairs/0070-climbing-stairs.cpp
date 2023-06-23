15f364ee347f7f37380275c24555bb66d207b08a
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-06-22 18:54:48 -0700

    Time: 0 ms (100.00%), Space: 6.2 MB (36.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+//DP problem
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+       if(n==1)
+       {
+           return 1;
+       }
+       vector<int> ans(n+1,0);
+        ans[1]=1;
+        ans[2]=2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+        return ans[n];
+    }
+};
