39b92afbfa68404f54b7729d33c37939590ed524
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-13 11:53:41 -0700

    Time: 7 ms (74.07%), Space: 13.8 MB (18.10%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,19 @@
+//Dynamic Programming
+//Time Complexity=O(N)
+//Space Complexity=O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi = -1e9;
+	    int pref = 1, suff = 1;
+        int n = nums.size();
+	    for(int i = 0; i < n; i++) {
+	        if(pref == 0) pref = 1;
+	        if(suff == 0) suff = 1;
+	        pref *= nums[i];
+	        suff *= nums[n - i - 1];
+	        maxi = max(maxi, max(pref, suff));
+	    }
+	    return maxi;
+    }
+};
