6c5cc0729e9c31c8ee6b6b56588a4b86807e0663
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-15 23:04:54 -0700

    Time: 33 ms (95.97%), Space: 12.3 MB (76.84%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+//time complexity=O(rc) r=row,c=columns
+//space complexity=O(rc)
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid,int x,int y)
+    {
+        //checking the constraints and if the cell is not one
+        if(x < 0 || y < 0 || x >= grid.size() || y >=grid[0].size() || grid[x][y]!='1')
+        {
+            return;
+        }
+        //make the current cell zero to mark it visited (in order to not visit it again)
+        grid[x][y]=0;
+        
+        //travel all 4 directions adjacent to the cell(dfs)
+        dfs(grid,x-1,y);
+        dfs(grid,x+1,y);
+        dfs(grid,x,y-1);
+        dfs(grid,x,y+1); 
+    }  
+        
+    int numIslands(vector<vector<char>>& grid) 
+    { 
+        int n=grid.size();   //row size
+        int m=grid[0].size();   //column size
+        
+        int no_of_islands=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    no_of_islands++;
+                    dfs(grid,i,j);   //check the neighboring cells to club them together
+                }
+            }
+        }
+        return no_of_islands;
+    }
+};
