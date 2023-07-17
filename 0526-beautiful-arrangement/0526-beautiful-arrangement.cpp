016e67fe9c4158bf2917ee342dd4e04907eae021
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-17 16:29:14 -0700

    Time: 128 ms (46.18%), Space: 6 MB (83.30%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,13 +1,18 @@
+//time complexity = O(n!)
+//space complexity = O(n)
+//recursion method
 class Solution 
 {
 public:
     void solve(int ind,vector<int> &visited,int n,int &ans)
     {
-        if(ind == n)
+        //base case
+        if(ind == n) // when the current Number becomes greater than the N means all places are filled which indicate we go the answer
         {
             ans++;
             return;
         }
+        //we have to iterate through all the positions
         for(int num =1;num<=n;num++)
         {
             if(!visited[num])
@@ -15,7 +20,9 @@ public:
                 if((ind+1) % num == 0 or num %(ind+1) == 0)
                 {
                     visited[num]=1;
+                    //recursive call
                     solve(ind+1,visited,n,ans);
+                    //backtrack
                     visited[num]=0;
                 }
             }
@@ -24,8 +31,10 @@ public:
     }
     int countArrangement(int n) 
     {
+        //first we want an array whose index will start from 1
         vector<int> visited(n+1,0);  //initialize all visited with 0
         int ans=0;
+        // this 1  is the Current Number we are talking
         solve(0,visited,n,ans);
         return ans;
     }
