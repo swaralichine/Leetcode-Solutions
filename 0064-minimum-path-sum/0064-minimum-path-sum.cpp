b0779618897d2ac6c90e6bbcac6304cfdbc40016
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-18 11:10:26 -0700

    Time: 12 ms (55.87%), Space: 9.6 MB (95.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,5 +1,5 @@
 //Time Complexity=O(row*column)
-//Space Complexity=O(row*column)
+//Space Complexity=O(1)
 //DP
 class Solution 
 {
@@ -12,12 +12,16 @@ public:
         int m=grid[0].size();
         
 
-        for (int i = grid.size() - 1; i >= 0; i--) {
-        for (int j = grid[0].size() - 1; j >= 0; j--) {
+        for (int i = grid.size() - 1; i >= 0; i--) 
+        {
+        for (int j = grid[0].size() - 1; j >= 0; j--) 
+        {
             if (i == grid.size() - 1 && j != grid[0].size() - 1)
                 grid[i][j] = grid[i][j] + grid[i][j + 1];
+            
             else if (j == grid[0].size() - 1 && i != grid.size() - 1)
                 grid[i][j] = grid[i][j] + grid[i + 1][j];
+            
             else if (j != grid[0].size() - 1 && i != grid.size() - 1)
                 grid[i][j] = grid[i][j] + std::min(grid[i + 1][j], grid[i][j + 1]);
         }
