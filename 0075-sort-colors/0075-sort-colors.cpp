c868d249118421d65332d98c7d486990bf77ce6c
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-17 19:53:51 -0700

    Time: 4 ms (25.29%), Space: 8.3 MB (38.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,37 +1,31 @@
 //time complexity=O(n)
 //space complexity=O(1)
 
-class Solution 
-{
+class Solution {
 public:
-    void sortColors(vector<int>& nums) 
-    {
-        int red = 0, white = 0, blue = 0, n = nums.size();
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+                continue;
+            }
+            if(nums[mid]==1){
+                mid++;
+                continue;
+            }
+            if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;  
+                continue;
+            }
+            
+        }
 
-        //Initialising the count of all three colors in 3 variables
-        for(int i =0; i < n ; i++)
-        {
-            if(nums[i] == 0) 
-                red++;
-            else if(nums[i] == 1)
-                white++;
-            else 
-                blue++;
-        }
-        nums.clear(); // clearing the nums
-
-        // inserting the values to nums in sorted order
-        for(int i =0 ;i < red; i++)
-        {
-            nums.emplace_back(0);
-        }
-        for(int i =0 ;i < white; i++)
-        {
-            nums.emplace_back(1);
-        }
-        for(int i =0 ;i < blue; i++)
-        {
-            nums.emplace_back(2);
-        }
     }
 };
+//used National Dutch Flag ALgorithm
