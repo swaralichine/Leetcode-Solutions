2f29ec2a9271ec6466cac1f64effcb6980c1bfb4
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-18 11:49:25 -0700

    Time: 57 ms (77.78%), Space: 13.9 MB (77.78%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     unordered_map<string, int> mem;
+    
     int minKnightMoves(int x, int y) 
     {
         x = abs(x); y = abs(y);
         auto key = to_string(x)+" "+to_string(y);
-        if (mem.count(key)) return mem[key];
-        if (x + y == 0) return 0;
-        else if (x + y == 2) return 2;
+        if (mem.count(key)) 
+            return mem[key];
+        if (x + y == 0)
+            return 0;
+        else if (x + y == 2)
+            return 2;
         return mem[key] = 1 + min(minKnightMoves(x-1, y-2), minKnightMoves(x-2, y-1));
     }
 };
