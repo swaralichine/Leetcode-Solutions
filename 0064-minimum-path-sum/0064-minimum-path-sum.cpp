d15c7494bd90059bb3b0899e97852750e1b55bd7
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-18 11:11:44 -0700

    Time: 12 ms (55.87%), Space: 9.6 MB (87.53%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -16,10 +16,10 @@ public:
         {
         for (int j = grid[0].size() - 1; j >= 0; j--) 
         {
-            if (i == grid.size() - 1 && j != grid[0].size() - 1)
+            if (i == grid.size() - 1 && j != grid[0].size() - 1) //last row
                 grid[i][j] = grid[i][j] + grid[i][j + 1];
             
-            else if (j == grid[0].size() - 1 && i != grid.size() - 1)
+            else if (j == grid[0].size() - 1 && i != grid.size() - 1)  //last column
                 grid[i][j] = grid[i][j] + grid[i + 1][j];
             
             else if (j != grid[0].size() - 1 && i != grid.size() - 1)
