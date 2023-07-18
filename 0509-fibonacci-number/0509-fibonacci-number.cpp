6a4ae8d5102895dafb777bf7f06d4521f57077ff
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-07-17 20:07:13 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (24.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,12 @@
-// Normal Recursive approach
-
 class Solution {
-    public:
+public:
     int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
-}
-    };
+        vector<int>dp(n+1);
+        dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
