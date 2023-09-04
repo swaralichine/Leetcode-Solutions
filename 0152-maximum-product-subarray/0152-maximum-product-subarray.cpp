dc0f5678870ead284698838f7e483e150f95b37d
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-04 10:36:20 -0700

    Time: 7 ms (49.94%), Space: 13.8 MB (67.81%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,4 +1,8 @@
-//Dynamic Programming
+// We can solve it using dynamic programming. If it was a maximum sum problem, we would have stored the minimum sum seen so far and subtract that from the current sum to get the maximum sum at that index.
+
+// For finding the maximum product, since all the numbers are integer, so we know that the product would be maximum when either we multiply the negative number with a negative number or positive number with a positive number.
+
+// If the negative number comes, then we should swap our maximum productn with minimum, because it will now make the maximum more less. For example, maxi=4 and mini=-2 and nums[i]=-2., then the larger number would now become smaller then smaller number.
 //Time Complexity=O(N)
 //Space Complexity=O(1)
 class Solution 
