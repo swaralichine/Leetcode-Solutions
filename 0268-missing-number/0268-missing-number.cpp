0e6e12be748e55bd8e0732f4208c1e34cf1bd492
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-14 09:58:21 -0700

    Time: 15 ms (66.62%), Space: 18.3 MB (8.88%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,20 @@
+//time complexity=O(n)
+//Space complexity=O(1)
+//use gauss formula
+class Solution 
+{
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int ans=0;
+        int n=nums.size();
+        int expected_sum = n*(n+1)/2;
+        
+        for(int i=0;i<n;i++)
+        {
+            ans = ans + nums[i];
+        }
+        
+        return expected_sum-ans;
+    }
+};
