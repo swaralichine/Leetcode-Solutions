ab73a1a96a0644918111e4d189a4666f58f51d91
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-14 21:56:24 -0700

    Time: 10 ms (20.08%), Space: 14 MB (5.51%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -13,14 +13,17 @@ public:
      int r=nums[0];
      int maxi=r;
      int mini=r;
+        
         for(int i=1;i<nums.size();i++)
         {
             if(nums[i]<0)
             {
+                //swap logic
                 int temp=maxi;
                 maxi=mini;
                 mini=temp;
             }
+            
             maxi=max(nums[i],nums[i]*maxi);
             mini=min(nums[i],nums[i]*mini);
             r=max(r,maxi);
