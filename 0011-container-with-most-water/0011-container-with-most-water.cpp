d112ffaf6b605b0db99a00ad84989cdec401f182
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-14 22:57:08 -0700

    Time: 58 ms (94.57%), Space: 59.2 MB (6.23%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,29 @@
+//Two Pointer Approach
+//time complexity=O(n)
+//Space complexity=O(1)
+class Solution 
+{
+public:
+    int maxArea(vector<int>& height) 
+    {
+       int ans=0;
+       int left=0;
+       int right=height.size()-1;
+
+       while(left < right)
+       {
+          int index_diff = right - left;  //these are the indices
+          ans=max(ans,min(height[left],height[right])*index_diff);  //logic
+           
+          if(height[right] > height[left])
+          {
+              left++;
+          }
+          else
+          {
+              right--;
+          }
+       }
+       return ans;
+    }
+};
