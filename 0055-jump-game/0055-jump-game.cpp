2983675a7be0fb913a6be5144b1d98b5ce59d152
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-15 17:15:02 -0700

    Time: 53 ms (48.53%), Space: 48.7 MB (21.20%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,21 @@
+//Valley peak approach
+//Time Complexity=O(N)
+//Space Complexity=O(1)
+class Solution 
+{
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int max_reachable=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(max_reachable < i)  //when the pointer has moved ahead
+            {
+                return false;
+            }
+            max_reachable=max(max_reachable,nums[i]+i);
+        }
+        return true;
+    }
+};
