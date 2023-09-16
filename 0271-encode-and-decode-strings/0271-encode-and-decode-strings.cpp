c6b2d50b95ea2f60776bb680610938206b6f8de0
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-16 16:15:14 -0700

    Time: 56 ms (8.13%), Space: 145.9 MB (5.05%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
@@ -1,53 +1,35 @@
-//we take delimiter as say #
-//but since string can have any ascii character including #
-//we store (length of the string + # + string)
-//this will ensure that the first # is not part of string, and length given will determine where the string starts and finishes
+
 class Codec 
 {
 public:
-
     // Encodes a list of strings to a single string.
-    string encode(vector<string>& strs)
+    string encode(vector<string>& strs) 
     {
-        string encoded = "";
-        int n = strs.size();
-        for(int i=0;i<n;i++)
+        string s;
+        for (string& str : strs) 
         {
-            int len = strs[i].size();
-            encoded = encoded + (to_string(len) + '#' + strs[i]);
+            s.push_back(str.size() - 100);
+            s = s + str;
         }
-        return encoded;
+        return s;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s)
     {
-        vector<string> decoded_string;
-        int n = s.length();
-        int i=0;
-        
-        while(i<n)
+        int n = s.size();
+        vector<string> strs;
+        int i = 0;
+        while (i < n) 
         {
-            string count="";
-            while(i<n && s[i]!='#')
-            {
-                count = count + s[i];
-                i++;
-            }
-            if(i>=n)
-            {
-                break;
-            }
-            i++; //to get character after #
-            int len = stoi(count);  //stoi = string to integer converter
-            string currstr = s.substr(i,len);
-            decoded_string.push_back(currstr);
-            i = i+len;
+            int strN = s[i++] + 100;
+            string str = s.substr(i, strN);
+            strs.push_back(str);
+            i += strN;
         }
-        return decoded_string;
+        return strs;
     }
 };
-
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.decode(codec.encode(strs));
