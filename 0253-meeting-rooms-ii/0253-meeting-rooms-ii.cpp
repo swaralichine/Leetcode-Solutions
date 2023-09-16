61c1006052c041eb99f53dbf37d1fa336305f7f6
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-15 18:41:54 -0700

    Time: 15 ms (63.31%), Space: 12.3 MB (48.44%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,13 +1,18 @@
 //Time Complexity=O(nlogn) (for sorting)
 //Space Complexity=O(n)  (for min heap)
-//basically need to find how many intervals are overlapping 
-class Solution {
+class Solution 
+{
 public:
-    int minMeetingRooms(vector<vector<int>>& intervals) {
+    int minMeetingRooms(vector<vector<int>>& intervals) 
+    {
         sort( intervals.begin() , intervals.end() );
-        priority_queue < int , vector < int > , greater< int >> q;
+        
+        priority_queue < int , vector < int > , greater< int >> q;   //min-heap
+        
         int n = intervals.size();
+        
         q.push(intervals[0][1]);
+        
         for(int i=1;i<n;i++)
         {
             int t = q.top();
