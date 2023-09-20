e0e7190e852afb687e4a128e4e09a319757fdc95
Swarali Chine <108090459+swaralichine@users.noreply.github.com>
2023-09-20 14:07:08 -0700

    Time: 11 ms (88.31%), Space: 11.9 MB (74.71%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -11,16 +11,16 @@ public:
         
         int n = intervals.size();
         
-        q.push(intervals[0][1]);
+        q.push(intervals[0][1]);   //end time
         
         for(int i=1;i<n;i++)
         {
             int t = q.top();
-            if( intervals[i][0]>=t)
+            if( intervals[i][0]>=t)    //start time
             {
                 q.pop();
             }
-            q.push(intervals[i][1]);
+            q.push(intervals[i][1]);  //again store the end time
         }
         return q.size();
     }
